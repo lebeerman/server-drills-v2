fff25cb1078f23bf7c9e32de96eec252a5479f1f
dan <omnidbeerman@gmail.com>
2017-11-14 12:22:09 -0700

    corrected status code assignments.

--- app.js
+++ app.js
@@ -1,81 +1,79 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 var app = express();
 app.use(cors());
-
 app.set('port', (process.env.PORT || 3000));
-
 const data = [
   {
     id: 1,
-    fullName: "Kyle Coberly",
-    title: "Faculty Director",
+    fullName: 'Kyle Coberly',
+    title: 'Faculty Director',
     numberOfDogs: 0
   },
   {
     id: 2,
-    fullName: "Danny Fritz",
-    title: "Lead Instructor",
+    fullName: 'Danny Fritz',
+    title: 'Lead Instructor',
     numberOfDogs: 0
   },
   {
     id: 3,
-    fullName: "CJ Reynolds",
-    title: "Lead Instructor",
+    fullName: 'CJ Reynolds',
+    title: 'Lead Instructor',
     numberOfDogs: 0
   },
   {
     id: 4,
-    fullName: "Brooks Patton",
-    title: "Lead Instructor",
+    fullName: 'Brooks Patton',
+    title: 'Lead Instructor',
     numberOfDogs: 0
   },
   {
     id: 5,
-    fullName: "Roberto Ortega",
-    title: "Lead Instructor",
+    fullName: 'Roberto Ortega',
+    title: 'Lead Instructor',
     numberOfDogs: 1
   },
   {
     id: 6,
-    fullName: "Chad Drummond",
-    title: "Instructor",
+    fullName: 'Chad Drummond',
+    title: 'Instructor',
     numberOfDogs: 0
   },
   {
     id: 7,
-    fullName: "Kim Schlesinger",
-    title: "Instructor",
+    fullName: 'Kim Schlesinger',
+    title: 'Instructor',
     numberOfDogs: 0
   },
   {
     id: 8,
-    fullName: "Peter Ostiguy",
-    title: "Associate Instructor",
+    fullName: 'Peter Ostiguy',
+    title: 'Associate Instructor',
     numberOfDogs: 1
   },
   {
     id: 9,
-    fullName: "Cass Torske",
-    title: "Resident",
+    fullName: 'Cass Torske',
+    title: 'Resident',
     numberOfDogs: 1
   },
   {
     id: 10,
-    fullName: "Matt Winzer",
-    title: "Resident",
+    fullName: 'Matt Winzer',
+    title: 'Resident',
     numberOfDogs: 2
   },
   {
     id: 11,
-    fullName: "Aaron Goodman",
-    title: "Resident",
+    fullName: 'Aaron Goodman',
+    title: 'Resident',
     numberOfDogs: 0
   },
   {
     id: 12,
-    fullName: "Michelle Bergquist",
-    title: "Resident",
+    fullName: 'Michelle Bergquist',
+    title: 'Resident',
     numberOfDogs: 1
   }
 ];
@@ -87,24 +85,23 @@ function getID(array, id) {
   }
   return false;
 }
-app.get("/", function(request, response){
+app.get('/', function(request, response){
   response.json(data);
 });
-app.get("/:id", function(request, response){
+app.get('/:id', function(request, response){
   if (getID(data,request.params.id)){
-  response.json(getID(data,request.params.id));
-  response.status = 200;
+    response.json(getID(data,request.params.id));
+    response.status(200);
   }
   else {
-    response.status = 404;
+    response.status(404);
     response.json({
-        error: {
-            message: "No record found!"
-        }
-      });
+      error: {
+        message: 'No record found!'
+      }
+    });
   }
 });
-
 app.listen(app.get('port'), function() {
   console.log('Node app v2 is running on port', app.get('port'));
 });
